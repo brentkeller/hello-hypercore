3ee2151ef41d222a0b80c31e89042aa23c64a8dc
Brent Keller <brentkeller@gmail.com>
2019-06-11 14:40:28 -0400

    More testing with idb and keys

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import hypercore from 'hypercore';
-import ram from 'random-access-memory';
+//import ram from 'random-access-memory';
 import rai from 'random-access-idb';
+import { Buffer } from 'buffer';
+
+const key = Buffer.from('1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef');
+const secretKey = Buffer.from('1234567890abcdef1234567890abcdef1234567890abcdef1234567890fedcba');
 
 const todos = rai('todos');
 
 const storage = (filename: any) => todos(filename);
 
 const feed = new hypercore(storage);
+// const feed = new hypercore(storage, key, { secretKey });
 
 
 // feed.append('hello world');
 
 feed.on('ready', () => {
-  console.log('ready');
+  console.log('ready', feed.key.toString('hex'));
+
   // feed.get(0, (err: any, d: any) => console.log(d.toString()));
   // feed.get(1, (err: any, d: any) => console.log(d.toString()));
 
